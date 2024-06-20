bd58a5d15acbbed5e46ffade5a29c751990ea674
Daniel <daniel.minior.pk@gmail.com>
2024-06-20 18:00:15 +0200

    First commit

---
+++ backendSCM/src/main/java/pl/dminior/backendSCM/BackendScmApplication.java
@@ -0,0 +1,13 @@
+package pl.dminior.backendSCM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendScmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendScmApplication.class, args);
+	}
+
+}
---
+++ backendSCM/src/test/java/pl/dminior/backendSCM/BackendScmApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dminior.backendSCM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendScmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
